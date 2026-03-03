42fe383bfd64e0b70a375b1d4559da0cbf415c5c
Iwan Bagus S <stywn93@gmail.com>
2026-03-03 07:46:24 +0700

    initial commit

---
+++ ToDo.md
@@ -0,0 +1,3 @@
+# To Do
+- Call API Login
+- Save API Key to Local Storage
---
+++ src/lib/api/User.js
@@ -0,0 +1,73 @@
+export const userRegister = async ({username, password, name}) => {
+    return await fetch(`${import.meta.env.VITE_API_PATH}/users`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            username, password, name
+        })
+    })
+}
+
+export const userLogin = async ({email, password}) => {
+    return await fetch(`${import.meta.env.VITE_API_PATH}/auth/login`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            email, password
+        })
+    })
+}
+
+export const userUpdateProfile = async (token, {name}) => {
+    return await fetch(`${import.meta.env.VITE_API_PATH}/users/current`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': token
+        },
+        body: JSON.stringify({
+            name
+        })
+    })
+}
+
+export const userUpdatePassword = async (token, {pass}) => {
+    return await fetch(`${import.meta.env.VITE_API_PATH}/users/current`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': token
+        },
+        body: JSON.stringify({
+            pass
+        })
+    })
+}
+
+export const userDetail = async (token) => {
+    return await fetch(`${import.meta.env.VITE_API_PATH}/users/current`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': token
+        }
+    })
+}
+
+export const userLogout = async (token) => {
+    return await fetch(`${import.meta.env.VITE_API_PATH}/users/logout`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': token
+        }
+    })
+}
