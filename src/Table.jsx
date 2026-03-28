39ea3469f67d625c3eb20299e176ba2a5852f281
Iwan Bagus S <stywn93@gmail.com>
2026-03-28 11:17:48 +0700

    WIP table data

--- src/Table.jsx
+++ src/Table.jsx
@@ -1,14 +1,24 @@
-import { useReactTable, getCoreRowModel, flexRender } from '@tanstack/react-table'
-const data = [{ id: 1, name: 'Ada' }]
-const columns = [{ accessorKey: 'name', header: 'Name' }]
+import {useReactTable, getCoreRowModel, flexRender} from '@tanstack/react-table'
+
+const data = [
+    {id: 1, name: 'Ada', age: 12},
+    {id: 2, name: 'Bat', age: 12},
+    {id: 3, name: 'Cat', age: 12},
+    {id: 4, name: 'Dog', age: 12},
+]
+const columns = [
+    {accessorKey: 'name', header: 'Name'},
+    {accessorKey: 'age', header: 'Age'},
+    {accessorKey: 'age', header: 'Age'},
+]
 
 export default function Table(props) {
-    const table = useReactTable({ data, columns, getCoreRowModel: getCoreRowModel() })
+    const table = useReactTable({data, columns, getCoreRowModel: getCoreRowModel()})
     return (
         <table className={"w-full text-sm text-left rtl:text-right text-body"}>
             <thead className={"bg-neutral-secondary-soft border-b border-default"}>
             {table.getHeaderGroups().map((hg) => (
-                <tr key={hg.id} >
+                <tr key={hg.id}>
                     {hg.headers.map((header) => (
                         <th key={header.id} className={"px-6 py-3 font-medium"}>
                             {flexRender(header.column.columnDef.header, header.getContext())}
@@ -19,7 +29,8 @@ export default function Table(props) {
             </thead>
             <tbody>
             {table.getRowModel().rows.map((row) => (
-                <tr key={row.id} className={"odd:bg-neutral-primary even:bg-neutral-secondary-soft border-b border-default"}>
+                <tr key={row.id}
+                    className={"odd:bg-neutral-primary even:bg-neutral-secondary-soft border-b border-default"}>
                     {row.getVisibleCells().map((cell) => (
                         <td key={cell.id} className={"px-6 py-4 font-medium text-heading whitespace-nowrap"}>
                             {flexRender(cell.column.columnDef.cell, cell.getContext())}
